c1565b82658eefd4b1b965aa8d6b9a9620ff2560
artrsyf <rom.fadeev2017@yandex.ru>
2024-11-15 23:46:38 +0300

    Implement workpiece for CMD parsing

---
+++ src/main/scala/loganalyzer/application/Parser/CommandLineParser/CommandLineParser.scala
@@ -0,0 +1,53 @@
+package loganalyzer.application.parser.CommandLineParser
+
+import scopt.OParser
+
+case class Config(
+  path: String = "",
+  from: Option[String] = None,
+  format: String = "text"
+)
+
+object CommandLineParser {
+
+  def run(args: Array[String]): Either[Error, Config] = {
+    // Конфигурация для парсинга аргументов
+    val builder = OParser.builder[Config]
+    val parser = {
+      import builder._
+      OParser.sequence(
+        programName("analyzer"),
+        head("Analyzer", "1.0"),
+
+        opt[String]("path")
+          .required()
+          .valueName("<path>")
+          .action((x, c) => c.copy(path = x))
+          .text("Путь к логам, поддерживает шаблоны, например: logs/2024*"),
+
+        opt[String]("from")
+          .optional()
+          .valueName("<date>")
+          .action((x, c) => c.copy(from = Some(x)))
+          .text("Начальная дата в формате YYYY-MM-DD"),
+
+        opt[String]("format")
+          .optional()
+          .valueName("<format>")
+          .action((x, c) => c.copy(format = x))
+          .text("Формат вывода: text или markdown")
+      )
+    }
+
+    // Парсинг аргументов и запуск программы
+    OParser.parse(parser, args, Config()) match {
+      case Some(config) =>
+        // Если парсинг успешен, запускаем анализ логов
+        Right(config)
+
+      case _ =>
+        Left(Error("Mismatch"))
+        // Если парсинг неуспешен, scopt автоматически выведет сообщение об ошибке
+    }
+  }
+}
