1412be77aa096c8e86ece89d793a5a7f66b6645c
artrsyf <rom.fadeev2017@yandex.ru>
2024-11-17 20:30:48 +0300

    FIX: Fix test format through scalafmt

--- src/test/scala/loganalyzer/application/NginxLogAnalyzer/NginxLogRecord/NginxLogRecordSpec.scala
+++ src/test/scala/loganalyzer/application/NginxLogAnalyzer/NginxLogRecord/NginxLogRecordSpec.scala
@@ -10,18 +10,24 @@ import loganalyzer.shared.enums.Http.RequestMethod
 
 class NginxLogRecordSpec extends AnyFunSuite:
   test("NginxLogRecord.newLogRecordFromString success"):
-    val nginxLog = "117.34.196.86 - - [13/Nov/2024:17:16:08 +0000] \"GET /installation%20Extended/frame/bifurcated-Down-sized.hmtl HTTP/1.1\" 200 2067 \"-\" \"Mozilla/5.0 (Windows 98) AppleWebKit/5360 (KHTML, like Gecko) Chrome/40.0.867.0 Mobile Safari/5360\""
+    val nginxLog =
+      "117.34.196.86 - - [13/Nov/2024:17:16:08 +0000] \"GET /installation%20Extended/frame/bifurcated-Down-sized.hmtl HTTP/1.1\" 200 2067 \"-\" \"Mozilla/5.0 (Windows 98) AppleWebKit/5360 (KHTML, like Gecko) Chrome/40.0.867.0 Mobile Safari/5360\""
 
     val record = NginxLogRecord.newLogRecordFromString(nginxLog)
 
     assert(record.remoteAddress == "117.34.196.86")
 
-    val expectedDate = OffsetDateTime.parse("13/Nov/2024:17:16:08 +0000", DateTimeFormatter.ofPattern("d/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH))
+    val expectedDate = OffsetDateTime.parse(
+      "13/Nov/2024:17:16:08 +0000",
+      DateTimeFormatter.ofPattern("d/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH)
+    )
     assert(record.requestTimeStamp == expectedDate)
 
     assert(record.requestMethod == RequestMethod.GET)
 
-    assert(record.requestUrl == "/installation%20Extended/frame/bifurcated-Down-sized.hmtl")
+    assert(
+      record.requestUrl == "/installation%20Extended/frame/bifurcated-Down-sized.hmtl"
+    )
 
     assert(record.httpVersion == "HTTP/1.1")
 
