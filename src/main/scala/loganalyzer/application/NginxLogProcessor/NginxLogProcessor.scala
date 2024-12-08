70ea7b4e64615b647ce67c31b81eef8f77f97bc9
artrsyf <rom.fadeev2017@yandex.ru>
2024-12-09 00:03:29 +0300

    Format code through scalafmt

--- src/main/scala/loganalyzer/application/NginxLogProcessor/NginxLogProcessor.scala
+++ src/main/scala/loganalyzer/application/NginxLogProcessor/NginxLogProcessor.scala
@@ -24,117 +24,124 @@ import loganalyzer.application.NginxLogAnalyzer.LogReport.ResourcesLogReport.Res
 import loganalyzer.shared.constants.Report.*
 
 class NginxLogProcessor(
-    config: Config,
-    fileReader: FileReader,
-    fileGenerator: FileGenerator,
-    filterFunction: NginxLogRecord => Boolean
+  config: Config,
+  fileReader: FileReader,
+  fileGenerator: FileGenerator,
+  filterFunction: NginxLogRecord => Boolean
 ):
-    private def filePath = config.path
+  private def filePath = config.path
 
-    private def reportFormat = config.format
+  private def reportFormat = config.format
 
-    private def fromDate = config.from match
-        case Some(value) =>
-            OffsetDateTime.parse(value, FormatterIso8601)
-        case _ =>
-            OffsetDateTime.MIN
+  private def fromDate = config.from match
+    case Some(value) =>
+      OffsetDateTime.parse(value, FormatterIso8601)
+    case _ =>
+      OffsetDateTime.MIN
 
-    private def toDate = config.to match
-        case Some(value) =>
-            OffsetDateTime.parse(value, FormatterIso8601)
-        case _ =>
-            OffsetDateTime.MAX
+  private def toDate = config.to match
+    case Some(value) =>
+      OffsetDateTime.parse(value, FormatterIso8601)
+    case _ =>
+      OffsetDateTime.MAX
 
-    private def emptyReportList = Array(
-          GeneralLogReport(),
-          ResponseCodesLogReport(),
-          UserAgentsLogReport(),
-          HttpMethodsLogReport(),
-          ResourcesLogReport(tenPopularRecordBound = true)
-        )
-    
-    def lazyReadFiles: IO[Iterator[String]] = {
-      fileReader.readFiles(filePath).flatMap { inputStreams =>
-        val resource = Resource.make(IO(inputStreams.iterator)) { _ =>
-          IO.unit
-        }
-        
-        resource.use { iterator =>
-          IO {
-            iterator.flatMap { inputStream =>
-              val reader = new BufferedReader(new InputStreamReader(inputStream))
-              Iterator.continually(reader.readLine()).takeWhile(_ != null).map(_.trim)
-            }
+  private def emptyReportList = Array(
+    GeneralLogReport(),
+    ResponseCodesLogReport(),
+    UserAgentsLogReport(),
+    HttpMethodsLogReport(),
+    ResourcesLogReport(tenPopularRecordBound = true)
+  )
+
+  def lazyReadFiles: IO[Iterator[String]] = {
+    fileReader.readFiles(filePath).flatMap { inputStreams =>
+      val resource = Resource.make(IO(inputStreams.iterator)) { _ =>
+        IO.unit
+      }
+
+      resource.use { iterator =>
+        IO {
+          iterator.flatMap { inputStream =>
+            val reader = new BufferedReader(new InputStreamReader(inputStream))
+            Iterator
+              .continually(reader.readLine())
+              .takeWhile(_ != null)
+              .map(_.trim)
           }
         }
       }
     }
+  }
 
-    def logRecordInvariant(logRecord: NginxLogRecord): Boolean = 
-        (
-            logRecord.requestTimeStamp.isAfter(fromDate) ||
-            logRecord.requestTimeStamp.isEqual(fromDate)
-        ) &&
-        (
-            logRecord.requestTimeStamp.isBefore(toDate) ||
-            logRecord.requestTimeStamp.isEqual(toDate)
-        ) &&
-        filterFunction(logRecord)
+  def logRecordInvariant(logRecord: NginxLogRecord): Boolean =
+    (
+      logRecord.requestTimeStamp.isAfter(fromDate) ||
+        logRecord.requestTimeStamp.isEqual(fromDate)
+    ) &&
+      (
+        logRecord.requestTimeStamp.isBefore(toDate) ||
+          logRecord.requestTimeStamp.isEqual(toDate)
+      ) &&
+      filterFunction(logRecord)
 
-    def updateReportsAfterProcessing(initReports: Array[LogReport]): IO[Array[LogReport]] = 
-      lazyReadFiles.flatMap { logLinesIterator =>
-        fileReader.readFileNames(filePath).flatMap { fileNames =>
-          val updatedReports = initReports.map {
-            case generalReport: GeneralLogReport =>
-              generalReport.copy(fileNames = fileNames)
-            case otherReport => otherReport
-          }
+  def updateReportsAfterProcessing(
+    initReports: Array[LogReport]
+  ): IO[Array[LogReport]] =
+    lazyReadFiles.flatMap { logLinesIterator =>
+      fileReader.readFileNames(filePath).flatMap { fileNames =>
+        val updatedReports = initReports.map {
+          case generalReport: GeneralLogReport =>
+            generalReport.copy(fileNames = fileNames)
+          case otherReport => otherReport
+        }
 
-          logLinesIterator.foldLeft(IO.pure(updatedReports)) { (ioReports, logLine) =>
+        logLinesIterator.foldLeft(IO.pure(updatedReports)) {
+          (ioReports, logLine) =>
             ioReports.flatMap { reports =>
               val logRecord = NginxLogRecord(logLine)
 
-              val updatedReports = 
+              val updatedReports =
                 if (logRecordInvariant(logRecord)) {
-                  reports.map(report => report.updateWithSingleIteration(logRecord))
+                  reports
+                    .map(report => report.updateWithSingleIteration(logRecord))
                 } else {
                   reports
                 }
 
               IO.pure(updatedReports)
             }
-          }
         }
       }
-    
-    def assemblyReport(updatedReportsAfterProcessing: Array[LogReport]) = 
-        val (finalReport, finalReportName) = reportFormat match
-              case MarkdownFormatNaming =>
-                (
-                  updatedReportsAfterProcessing.foldLeft("") { (acc, current) =>
-                    acc + s"\n${current.generateMarkdownReport()}"
-                  },
-                  "report.md"
-                )
-              case AdocFormatNaming =>
-                (
-                  updatedReportsAfterProcessing.foldLeft("") { (acc, current) =>
-                    acc + s"\n${current.generateAsciidocReport()}"
-                  },
-                  "report.adoc"
-                )
-              case _ =>
-                (
-                  "Given unsupported report format.",
-                  "report_error_log.txt"
-                )
-        
-        (finalReport, finalReportName)
+    }
 
-    def generateReport = 
-        updateReportsAfterProcessing(emptyReportList).flatMap { readyReports =>
-            val (report, reportName) = assemblyReport(readyReports)
-            
-            fileGenerator
-              .createFile("./report_dist", reportName, report)
-        }
+  def assemblyReport(updatedReportsAfterProcessing: Array[LogReport]) =
+    val (finalReport, finalReportName) = reportFormat match
+      case MarkdownFormatNaming =>
+        (
+          updatedReportsAfterProcessing.foldLeft("") { (acc, current) =>
+            acc + s"\n${current.generateMarkdownReport()}"
+          },
+          "report.md"
+        )
+      case AdocFormatNaming =>
+        (
+          updatedReportsAfterProcessing.foldLeft("") { (acc, current) =>
+            acc + s"\n${current.generateAsciidocReport()}"
+          },
+          "report.adoc"
+        )
+      case _ =>
+        (
+          "Given unsupported report format.",
+          "report_error_log.txt"
+        )
+
+    (finalReport, finalReportName)
+
+  def generateReport =
+    updateReportsAfterProcessing(emptyReportList).flatMap { readyReports =>
+      val (report, reportName) = assemblyReport(readyReports)
+
+      fileGenerator
+        .createFile("./report_dist", reportName, report)
+    }
--- src/main/scala/loganalyzer/shared/configs/DateConfig.scala
+++ src/main/scala/loganalyzer/shared/configs/DateConfig.scala
@@ -4,10 +4,10 @@ import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 val FormatterIso8601 = DateTimeFormatter
-        .ofPattern("yyyy-MM-dd'T'HH:mm:ssX", Locale.ENGLISH)
+  .ofPattern("yyyy-MM-dd'T'HH:mm:ssX", Locale.ENGLISH)
 
 val NginxLogFormatter = DateTimeFormatter
-        .ofPattern("d/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH)
+  .ofPattern("d/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH)
 
 val ReportFormatter = DateTimeFormatter
-        .ofPattern("dd.MM.yyyy")
+  .ofPattern("dd.MM.yyyy")
